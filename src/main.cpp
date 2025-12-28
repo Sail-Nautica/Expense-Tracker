55100e2ad5a16d66d44a01a02e4a0f43694bbc7d
Amit Sail <amitsail2014@gmail.com>
2025-12-27 19:07:03 -0500

    Adding Structs to Main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+struct Transaction {    
+    string id;
+    int amount_cents;
+    string type;
+    string category_id;
+    string date;
+    string note;
+};
+
+struct Category {
+    int id;
+    string name;
+};
+
+int main(){
+    vector<Transaction> Transaction_list;
+
+    bool flag = true;
+
+    while(flag){
+
+
+
+
+
+        
+    }
+
+
+
+
+    return 0;
+}
